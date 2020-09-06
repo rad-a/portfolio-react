41a6ea2ccd5149bba1301d61546fd0d9b15e0fda
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-05 19:56:35 -0500

    Set up routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {  Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import NavPills from './components/NavPills';
+
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <NavPills />
+
+      <div>
+        <h1>This is the Root Component </h1>
+        {/* Using Switch to match only one route at a time. Negates the need for 'exact' keyword. */}
+        <Switch>
+          <Route path='/contact' component={Contact} />
+          <Route path='/projects' component={Projects} />
+          <Route path='/' component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
