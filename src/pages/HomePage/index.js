a6b07d54f8ad8a9fa53157c2749051ff9ee1aab2
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-07 18:48:17 -0500

    HomePage complete

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,10 +1,28 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import { BsCaretDownFill } from "react-icons/bs";
+
+import "./style.css";
 
 function HomePage(props) {
   return (
-    <div>
-      <h2>Home works!</h2>
-    </div>
+    <Container className="profileContainer p-0" fluid={true}>
+      <Row className="profileContent" noGutters={true}>
+        <Col id="profileText">
+          {props.title && <h1>{props.title}</h1>}
+          {props.subTitle && <h3>{props.subTitle}</h3>}
+          {props.introText && <h5>{props.introText}</h5>}
+          {props.actionText && (
+            <Button variant="outline-success">
+              <strong>{props.actionText}</strong>
+            </Button>
+          )}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
