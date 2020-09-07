f5f323243a0175eecf8913028cc456a89cd5f8a5
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-07 00:34:50 -0500

    Footer complete

---
+++ src/components/Footer/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { FaGithub, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
+
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer center mt-5 p-2">
+      <Container class="footerContainer" fluid={true}>
+        <Row className="p-3">
+          <Col className="footerContent d-flex p-0">
+            <h4>Find me on the web</h4>
+            <Row className="socialIcons">
+              <a href="https://www.linkedin.com/in/rachaeladu" target="_blank">
+                <FaLinkedin className="fa-2x m-2" />
+              </a>
+              <a href="https://github.com/rad-a" target="_blank">
+                <FaGithub className="fa-2x m-2" />
+              </a>
+              <a href="https://twitter.com/arachael_" target="_blank">
+                <FaTwitterSquare className="fa-2x m-2" />
+              </a>
+            </Row>
+            <small>
+              2020 &copy;{" "}
+              <a href="https://rad-a.github.io/react_portfolio/">Rachael Adu</a>
+            </small>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
