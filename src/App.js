cc905842c19606e2664c5cd6c9b2d378ffdf2675
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-07 01:22:52 -0500

    Routing complete

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import "./App.css";
 
 import TopNavbar from "./components/TopNavbar";
 import Footer from "./components/Footer";
+import ProjectsPage from "./pages/ProjectsPage";
+import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
 
 // import Home from './pages/Home';
 // import Projects from './pages/Projects';
@@ -24,12 +28,12 @@ class App extends Component {
       ],
       home: {
         title: "Hi, I'm Rachael",
-        subtitle: "Developer, Designer, and Tea Enthusiast.",
+        subTitle: "Developer, Designer, and Tea Enthusiast.",
         introText: "I am a web developer based in Austin, Texas.",
       },
       about: {
         title: "About",
-        subtitle: "My Skills",
+        subTitle: "My Skills",
       },
       projects: {
         title: "My Projects",
@@ -45,6 +49,42 @@ class App extends Component {
       <Router>
         <Container className="p-0" fluid={true}>
           <TopNavbar />
+          {/* <Switch>
+            <Route component={AboutPage} path="/about" />
+            <Route component={ProjectsPage} path="/projects" />
+            <Route component={ContactPage} path="/contact" /> */}
+
+          {/* Define routes and set state for each page. */}
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <HomePage
+                  title={this.state.home.title}
+                  subTitle={this.state.home.subTitle}
+                  introText={this.state.home.introText}
+                />
+              )}
+            />
+            <Route
+              path="/about"
+              render={() => (
+                <AboutPage
+                  title={this.state.about.title}
+                  subTitle={this.state.about.subTitle}
+                />
+              )}
+            />
+            <Route
+              path="/projects"
+              render={() => <ProjectsPage title={this.state.projects.title} />}
+            />
+            <Route
+              path="/contact"
+              render={() => <ContactPage title={this.state.contact.title} />}
+            />
+          </Switch>
           <Footer />
         </Container>
       </Router>
