f0c227e2585874d5d7173f3ed38f1ce66b09964c
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-06 22:15:25 -0500

    Navbar complete

---
+++ src/components/TopNavbar/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
+
+import "./style.css";
+
+function TopNavbar() {
+  return (
+    <Navbar className="navbar" expand="lg">
+      <Navbar.Brand className="navbar-brand" href="/">
+        RACHAEL ADU
+      </Navbar.Brand>
+      <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+      <Navbar.Collapse id="navbar-toggle">
+        <Nav className="ml-auto">
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+          <Link className="nav-link" to="/projects">
+            Projects
+          </Link>
+          <Link className="nav-link" to="/contact">
+            Contact
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default TopNavbar;
