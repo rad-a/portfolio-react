0b86c6a39e3ee0398fe979df2fd045894134e220
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-09 23:17:18 -0500

    About page complete

--- src/pages/AboutPage/index.js
+++ src/pages/AboutPage/index.js
@@ -1,10 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import Header from "../../components/Header";
+import Content from "../../components/Content";
+import DevSkillContainer from "../../components/DevSkillContainer";
 
 function AboutPage(props) {
   return (
-    <div>
-      <h2>About works!</h2>
-      
+    <div className="mainContent">
+      <Header title={props.title} />
+
+      <Content>
+        <p>
+          I have a background in community and data operations, and I am
+          passionate about creating user-friendly products and experiences.
+        </p>
+        <p>
+          I am currently studying Full Stack Web Development at The University
+          of Texas. I am excited to keep expanding my coding skills.
+        </p>
+        <p>
+          Outside of class and work, I can be found getting overly excited about
+          houseplants, meal prepping, brushing up on my foreign language skills,
+          or enjoying a beautiful Austin day outdoors.
+        </p>
+        <p>
+          Let me <Link to="./contact">help you</Link> build amazing products!
+        </p>
+      </Content>
+
+      <Header subTitle={props.subTitle} />
+
+      <Content>
+        <DevSkillContainer />
+      </Content>
     </div>
   );
 }
