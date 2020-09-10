73a604d0930dfeb2463346b46bcaac754b0908e4
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-09 23:08:45 -0500

    Added content component to handle centered content

---
+++ src/Assets/DevSkillsData.js
@@ -0,0 +1,75 @@
+import gitIcon from "../pages/AboutPage/icons/git.svg";
+import bootstrapIcon from "../pages/AboutPage/icons/bootstrap.svg";
+import expressIcon from "../pages/AboutPage/icons/express.svg";
+import herokuIcon from "../pages/AboutPage/icons/heroku.svg";
+import jsIcon from "../pages/AboutPage/icons/javascript.svg";
+import htmlIcon from "../pages/AboutPage/icons/html5.svg";
+import jQueryIcon from "../pages/AboutPage/icons/jquery.svg";
+import foundationIcon from "../pages/AboutPage/icons/foundation.svg";
+import githubIcon from "../pages/AboutPage/icons/github.svg";
+import mongodbIcon from "../pages/AboutPage/icons/mongodb.svg";
+import mysqlIcon from "../pages/AboutPage/icons/mysql.svg";
+import nodejsIcon from "../pages/AboutPage/icons/nodejs.svg";
+import reactIcon from "../pages/AboutPage/icons/react.svg";
+import cssjsIcon from "../pages/AboutPage/icons/css3.svg";
+
+
+
+export default [
+  {
+    id: 13,
+    name: reactIcon,
+  },
+  {
+    id: 5,
+    name: jsIcon,
+  },  {
+    id: 7,
+    name: jQueryIcon,
+  },
+  {
+    id: 6,
+    name: htmlIcon,
+  } ,  
+  {
+    id: 14,
+    name: cssjsIcon,
+  },
+  {
+    id: 1,
+    name: gitIcon,
+  },
+  {
+    id: 2,
+    name: bootstrapIcon,
+  },
+  {
+    id: 3,
+    name: expressIcon,
+  },
+  {
+    id: 4,
+    name: herokuIcon,
+  },
+ 
+
+  {
+    id: 8,
+    name: mysqlIcon,
+  },
+  {
+    id: 9,
+    name: nodejsIcon,
+  },
+  {
+    id: 10,
+    name: githubIcon,
+  },  {
+    id: 11,
+    name: mongodbIcon,
+  },  {
+    id: 12,
+    name: foundationIcon,
+  },
+ 
+];
