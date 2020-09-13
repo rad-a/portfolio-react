f10eb30aee4682e35f7ecde5dbfd23588e542d29
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-12 21:27:37 -0500

    Updated footer link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,26 +8,43 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer center mt-5 p-2">
-      <Container class="footerContainer" fluid={true}>
+    <footer className="footer center p-2">
+      <Container fluid={true}>
         <Row className="p-3">
           <Col className="footerContent d-flex p-0">
             <h4>Find me on the web</h4>
             <Row className="socialIcons">
-            {/* rel="noopener noreferrer" added to links with 'target="_blank"' to protect user against potential threats */}
-              <a href="https://www.linkedin.com/in/rachaeladu" target="_blank" rel="noopener noreferrer">
+              {/* rel="noopener noreferrer" added to links with 'target="_blank"' to protect user against potential threats */}
+              <a
+                href="https://www.linkedin.com/in/rachaeladu"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <FaLinkedin className="fa-2x m-2" />
               </a>
-              <a href="https://github.com/rad-a" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://github.com/rad-a"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <FaGithub className="fa-2x m-2" />
               </a>
-              <a href="https://twitter.com/arachael_" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://twitter.com/arachael_"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <FaTwitterSquare className="fa-2x m-2" />
               </a>
             </Row>
             <small>
               2020 &copy;{" "}
-              <a href="https://rad-a.github.io/react_portfolio/">Rachael Adu</a>
+              <a
+                href="https://github.com/rad-a/portfolio-react"
+                target="_blank"
+              >
+                Rachael Adu
+              </a>
             </small>
           </Col>
         </Row>
