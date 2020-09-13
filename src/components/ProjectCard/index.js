9eb05a752adad7e6c7fecc38aa4db368ec4ec791
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-12 21:29:23 -0500

    Project card complete

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,24 +1,30 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-// import projectData from "../../Assets/ProjectData";
+import Button from "react-bootstrap/Button";
 
+import "./style.css";
 
-function ProjectCard(props) {
+const MooCard = (props) => {
   return (
-    <Card>
-      <Card.Header>
-        <Card.Title> {props.name} </Card.Title>
-      </Card.Header>
+    <Card className="projectItem mb-4">
       <Card.Body>
-        {/* <Card.Title> {props.name} </Card.Title> */}
-        <Card.Image> {props.image} </Card.Image>
-        <Card.Title> {props.name} </Card.Title>
-        <Card.Text> {props.description} </Card.Text>
-
-        {/* <Card.Footer></Card.Footer> */}
+        <Card.Img src={props.image} className=" mb-3" />
+        <Card.Title>
+          {" "}
+          <h4 className="font-weight-bolder pt-1">{props.name}</h4>{" "}
+        </Card.Title>
+        <Card.Text className="border-top pt-3"> {props.description} </Card.Text>
+        <Button variant="secondary" href={props.repository} target="_blank">
+          {" "}
+          Github
+        </Button>{" "}
+        <Button variant="secondary" href={props.url} target="_blank">
+          {" "}
+          App
+        </Button>
       </Card.Body>
     </Card>
   );
-}
+};
 
-export default ProjectCard;
+export default MooCard;
