2d9ee5f45a2e3e53323fbcfe88649b6e1bda1a50
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-12 22:22:40 -0500

    Updates

---
+++ precache-manifest.453de2991d08d6cef6436bdc0b99abe8.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45be94c18933d3e45e1bf1588760d30f",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "4fab234be45ea82b0a81",
+    "url": "/react_portfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "dea79a6337ade39163b1",
+    "url": "/react_portfolio/static/css/main.e8b63f74.chunk.css"
+  },
+  {
+    "revision": "4fab234be45ea82b0a81",
+    "url": "/react_portfolio/static/js/2.3fe207a1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_portfolio/static/js/2.3fe207a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea79a6337ade39163b1",
+    "url": "/react_portfolio/static/js/main.9075f845.chunk.js"
+  },
+  {
+    "revision": "5397e927921dde71372c",
+    "url": "/react_portfolio/static/js/runtime-main.d05fa38a.js"
+  },
+  {
+    "revision": "557ab1b22166690888e2fb1a6fc13a0a",
+    "url": "/react_portfolio/static/media/RPG-vn.08e4334a.jpg"
+  },
+  {
+    "revision": "ece94356d34d7d7e77b6b531b1586f70",
+    "url": "/react_portfolio/static/media/TRG-output-HTML.111997c0.jpg"
+  },
+  {
+    "revision": "76d7c6aabe5de63af067918dc270986c",
+    "url": "/react_portfolio/static/media/bootstrap.76d7c6aa.svg"
+  },
+  {
+    "revision": "95af1cd2e338034bb1d5419c5115b59c",
+    "url": "/react_portfolio/static/media/burger-app-vn.8cc8218d.jpg"
+  },
+  {
+    "revision": "2ca884530d6e08f8540700e76c3a63cb",
+    "url": "/react_portfolio/static/media/css3.2ca88453.svg"
+  },
+  {
+    "revision": "9723de6b230d6571f6ca2f80fc3702c4",
+    "url": "/react_portfolio/static/media/dog-date-project.ccb6f99c.jpg"
+  },
+  {
+    "revision": "f1a0b0e9c7eb8a3294e84f11441de5df",
+    "url": "/react_portfolio/static/media/express.f1a0b0e9.svg"
+  },
+  {
+    "revision": "d4022cbd5b9d1167bb8b49711b2f6d15",
+    "url": "/react_portfolio/static/media/foundation.d4022cbd.svg"
+  },
+  {
+    "revision": "66e06c06b4b40acff6a6595e710b5006",
+    "url": "/react_portfolio/static/media/git.66e06c06.svg"
+  },
+  {
+    "revision": "fff793948eaad4a21b6fb5712888d62d",
+    "url": "/react_portfolio/static/media/github.fff79394.svg"
+  },
+  {
+    "revision": "239bab29d025a58b280c0986471ad6bb",
+    "url": "/react_portfolio/static/media/heroku.239bab29.svg"
+  },
+  {
+    "revision": "eb0bfe64ddcaa0b4c3be19b19541b584",
+    "url": "/react_portfolio/static/media/html5.eb0bfe64.svg"
+  },
+  {
+    "revision": "40aa52d08b5f2b9ed609923301e1b871",
+    "url": "/react_portfolio/static/media/javascript.40aa52d0.svg"
+  },
+  {
+    "revision": "8ec9bacf8bc78b8ad161b5e9a2a16e87",
+    "url": "/react_portfolio/static/media/jquery.8ec9bacf.svg"
+  },
+  {
+    "revision": "b1847af9e7cf00cbe2d44e9fa173d281",
+    "url": "/react_portfolio/static/media/mongodb.b1847af9.svg"
+  },
+  {
+    "revision": "c02ade79e7034732a856b802471a5f31",
+    "url": "/react_portfolio/static/media/mysql.c02ade79.svg"
+  },
+  {
+    "revision": "ccb43d3400fb05951c89ad3ba6acd9d5",
+    "url": "/react_portfolio/static/media/nodejs.ccb43d34.svg"
+  },
+  {
+    "revision": "64e7b17978c84bde78f834be6d7ce81e",
+    "url": "/react_portfolio/static/media/profile-bg.c9e3515f.jpg"
+  },
+  {
+    "revision": "e9f3d01c242e086f895bde131803b451",
+    "url": "/react_portfolio/static/media/react.e9f3d01c.svg"
+  },
+  {
+    "revision": "1765ad3aa4590cfd1a88f4572f6d98ea",
+    "url": "/react_portfolio/static/media/weather-dashboard.49dae26f.png"
+  },
+  {
+    "revision": "07bee6bf8b41fa980c19ffe3ee1327c9",
+    "url": "/react_portfolio/static/media/workday-scheduler.238ea497.jpg"
+  }
+]);
