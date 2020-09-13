a725cf92d612df06ca2ff3e162ac5fd27a1baed4
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-12 21:31:19 -0500

    Added animation to cards

--- src/components/ProjectContainer/index.js
+++ src/components/ProjectContainer/index.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-// import Row from "react-bootstrap/Row";
 import CardColumns from "react-bootstrap/CardColumns";
-// import Container from "react-bootstrap/Container";
+import { Trail } from "react-spring/renderprops";
 
 import projectData from "../../Assets/ProjectData";
 import ProjectCard from "../ProjectCard";
-
 
 class ProjectContainer extends Component {
   state = {
@@ -14,22 +12,31 @@ class ProjectContainer extends Component {
 
   render() {
     return (
-      // <Container>
-      <CardColumns>
-        {this.state.projectData.map(project => (
-          <ProjectCard
-            id={project.id}
-            key={project.id}
-            name={project.name}
-            image={project.image}
-            // url={project.url}
-            // repository={project.repository}
-            description={project.description}
-            // role={project.role}
-          />
-        ))}
-      </CardColumns>
-      // </Container>
+      <div>
+        <CardColumns>
+          <Trail
+            items={projectData}
+            keys={(project) => project.id}
+            from={{ transform: "translate3d(-20px,0,0)" }}
+            to={{ transform: "translate3d(0,0px,0)" }}
+          >
+            {(project) => (props) => (
+              <div style={props}>
+                <ProjectCard
+                  id={project.id}
+                  key={project.id}
+                  name={project.name}
+                  image={project.image}
+                  url={project.url}
+                  repository={project.repository}
+                  description={project.description}
+                  role={project.role}
+                />
+              </div>
+            )}
+          </Trail>
+        </CardColumns>
+      </div>
     );
   }
 }
