90595e01be9a34f3fa48b7817b3930cee8837cf0
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-13 18:32:55 -0500

    Increased portfolio div width

--- src/pages/ProjectsPage/index.js
+++ src/pages/ProjectsPage/index.js
@@ -1,16 +1,19 @@
 import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 import ProjectContainer from "../../components/ProjectContainer/";
 import Header from "../../components/Header";
-import Content from "../../components/Content";
 
 function ProjectsPage(props) {
   return (
     <div className="mainContent">
       <Header title={props.title} />
-      <Content>
-        <ProjectContainer />
-      </Content>
+      <Row className="justify-content-center py-5">
+        <Col lg={8} md={10} xs={12}>
+          <ProjectContainer />
+        </Col>
+      </Row>
     </div>
   );
 }
