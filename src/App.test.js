27a380493f6a625eef7898b6a694ad410e27425c
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-12 21:23:58 -0500

    Removed commets

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,13 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// {
+//   test: /\.(png|jpg|gif)$/,
+//   use: [
+//     {
+//       loader: 'file-loader',
+//       options: {},
+//     },
+//   ],
+// };
